b0bcee12a1a7e383f6bf9627737fef4171ce3028
Bruno Bertolini <dev@brunobertolini.com>
2019-02-28 17:36:35 -0300

    feat(shared): packer component

---
+++ packages/shared/doczrc.js
@@ -0,0 +1,41 @@
+const IS_PROD = process.env.NODE_ENV === 'production'
+
+export default {
+	typescript: true,
+	codeSandbox: true,
+	sourcemaps: IS_PROD,
+	propsParser: IS_PROD,
+	title: 'Steroids / Shared',
+	menu: ['Components'],
+	htmlContext: {
+		head: {
+			links: [
+				{
+					rel: 'stylesheet',
+					href: 'https://codemirror.net/theme/dracula.css',
+				},
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/css?family=Poppins:400,500',
+				},
+			],
+		},
+	},
+	themeConfig: {
+		codemirrorTheme: 'dracula',
+		colors: {
+			primary: '#0097e6',
+		},
+		styles: {
+			h2: {
+				margin: '60px 0 30px',
+			},
+			pre: {
+				border: '0',
+			},
+			paragraph: {
+				margin: '10px 0 20px 0',
+			},
+		},
+	},
+}
---
+++ packages/shared/src/Packer/index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { omit, pick } from 'ramda'
+
+interface PackerProps {
+	container: React.SFC<any>
+	containerProps: string[]
+	sharedProps: string[]
+	fixedProps: object
+}
+
+export const Packer: React.SFC<PackerProps> = ({
+	container: Container,
+	containerProps,
+	sharedProps,
+	fixedProps,
+	children,
+}) => (
+	<>
+		{React.Children.toArray(children)
+			.filter(React.isValidElement)
+			.map((child: any, idx: number) =>
+				child.type && child.type.name && child.type.name === Container.name ? (
+					child
+				) : (
+					<Container
+						{...fixedProps}
+						{...pick([...containerProps, ...sharedProps], child.props)}
+						key={idx}
+					>
+						{{ ...child, props: omit(containerProps, child.props) }}
+					</Container>
+				)
+			)}
+	</>
+)
+
+Packer.defaultProps = {
+	containerProps: [],
+	sharedProps: [],
+	fixedProps: {},
+}
