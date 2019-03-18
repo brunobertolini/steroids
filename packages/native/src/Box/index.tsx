4be8a4a9b9412c0ce76529a8a41a739f342f287f
Bruno Bertolini <dev@brunobertolini.com>
2019-03-18 10:50:13 -0300

    refactor(native): transform components to basic maped components

--- packages/native/src/Box/index.tsx
+++ packages/native/src/Box/index.tsx
@@ -1,10 +1,5 @@
-import * as React from 'react'
 import { View, ViewProps } from 'react-native'
 import styled from 'styled-components'
-import { types as t, withAll } from '@steroids/styled'
 
-export type BoxProps = ViewProps & t.AllProps
-
-export const Box: React.SFC<BoxProps> = styled(View)`
-	${withAll}
-`
+export type BoxProps = ViewProps
+export const Box = styled(View)<BoxProps>``
--- packages/native/src/Text/index.tsx
+++ packages/native/src/Text/index.tsx
@@ -1,14 +1,5 @@
-import * as React from 'react'
 import { Text as BaseText, TextProps as BaseTextProps } from 'react-native'
 import styled from 'styled-components'
-import { types as t, withText } from '@steroids/styled'
 
-export type TextProps = BaseTextProps & t.TextProps
-
-export const Text: React.SFC<TextProps> = styled(BaseText)`
-	${withText}
-`
-
-Text.defaultProps = {
-	uppercase: false,
-}
+export type TextProps = BaseTextProps
+export const Text = styled(BaseText)<TextProps>``
