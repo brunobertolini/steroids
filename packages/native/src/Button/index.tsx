0dd4d143586aa1ff5b9dc666494b0937b23b32b3
Bruno Bertolini <dev@brunobertolini.com>
2019-03-19 11:24:03 -0300

    feat(native): basic touchable button

--- packages/native/src/Button/index.tsx
+++ packages/native/src/Button/index.tsx
@@ -1,5 +1,17 @@
-import { View, ViewProps } from 'react-native'
 import styled from 'styled-components'
 
-export type ButtonProps = ViewProps
-export const Button = styled(View)<ButtonProps>``
+import {
+	Platform,
+	TouchableNativeFeedback,
+	TouchableNativeFeedbackProps,
+	TouchableOpacity,
+	TouchableOpacityProps,
+} from 'react-native'
+
+const Touchable =
+	Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
+
+type TouchableProps = TouchableNativeFeedbackProps & TouchableOpacityProps
+
+export type ButtonProps = TouchableProps
+export const Button = styled(Touchable)<ButtonProps>``
